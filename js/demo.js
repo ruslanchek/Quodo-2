69a19cb7218e65bc3f7a84a878e6e8dcc36a1be1
Ruslan Shashkov <rshashkov@icloud.com>
2015-01-23 12:07:49 +0300

    Trying to make a Popup

--- js/demo.js
+++ js/demo.js
@@ -4,14 +4,15 @@ var Demo = {};
 
 Demo.Table = function(){
 	this.initChecker = function(){
-		var checker = new UI.Checker('#table-checker',
-			function($checker){
+		var checker = new UI.Checker({
+			selector: '#table-checker',
+			onCheck: function($checker){
 				$('#table-sample').addClass($checker.data('type'));
 			}, 
-			function($checker){
+			onUncheck: function($checker){
 				$('#table-sample').removeClass($checker.data('type'));
 			}
-		);
+		});
 	};
 
 	this.init = function(){
@@ -21,14 +22,9 @@ Demo.Table = function(){
 
 Demo.Checker = function(){
     this.initChecker = function(){
-        var checker = new UI.Checker('#sample-checker',
-            function($checker){
-
-            },
-            function($checker){
-
-            }
-        );
+        var checker = new UI.Checker({
+        	selector: '#sample-checker'
+        });
     };
 
     this.init = function(){
--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,45 @@
-var UI = {};
+var UI = {
+	settings: {
+		templateSettings: {
+		  	interpolate: /\{\{(.+?)\}\}/g
+		}
+	}
+};
 
-UI.Checker = function(selector, check, uncheck){
-	var $checker = $(selector);
+UI.Template = function(templateName){
+	_.templateSettings = this.settings.templateSettings;
 
-	$checker.find('>a').off('click.UIChecker').on('click.UIChecker', function(e){
+	var getTemplate = function(){
+		var template = $('#' + templateName).html();
+
+		if(template){
+			return template;
+		}else{
+			console.error('UI.Template', 'Template is empty!');
+		}
+	};
+
+	this.render = function(data){
+		var template = _.template(getTemplate());
+
+		return template(data);
+	};
+};
+
+UI.Checker = function(options){
+	var _this = this;
+
+	this.options = $.extend({
+		selector: '.checker', 
+		onCheck: function($checker){
+
+		}, 
+		onUncheck: function($checker){
+
+		}
+	}, options);
+
+	$(this.options.selector).find('>a').off('click.UIChecker').on('click.UIChecker', function(e){
         e.preventDefault();
 
         var activeClass = '';
@@ -14,10 +50,68 @@ UI.Checker = function(selector, check, uncheck){
 
 		if($(this).hasClass('active')){
 			$(this).removeClass('active' + activeClass);
-			if(uncheck) uncheck($(this));
+			_this.options.onUncheck($(this));
 		}else{
 			$(this).addClass('active' + activeClass);
-			if(check) check($(this));
+			_this.options.onCheck($(this));
 		}
 	});
+};
+
+UI.Popup = function(options){
+	var _this = this;
+
+	this.options = $.extend({
+		templateSelector: '#template-ui-popup',
+		onShow: function(){
+
+		},
+		onHide: function(){
+
+		}
+	}, options);
+
+	var bind = function(){
+		unbind();
+
+		$(document).on('keyup.UIPopup', function(e){
+			switch(e.keyCode){
+				case 13 : {
+					this.hide();
+				} break;
+			}
+		});
+	};
+
+	var unbind = function(){
+		$(document).off('keyup.UIPopup');
+	};
+
+	var template = new UI.Template(_this.options.templateSelector);
+
+	var make = function(title, content){
+		return template.render({
+			title: title,
+			content: content
+		});
+	};
+
+	this.changeContent = function(html){
+
+	};
+
+	this.changeTitle = function(html){
+
+	};
+
+	this.show = function(title, content){
+		this.hide();
+		$('body').append(make(title, content));
+		this.options.onShow();
+	};
+
+	this.hide = function(){
+		unbind();
+		this.options.onHide();
+	};
 };
