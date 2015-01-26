f55d28ff2e11f6abab7d12c3e67ec61f90a7798a
Ruslan Shashkov <rshashkov@icloud.com>
2015-01-26 19:07:41 +0300

    Popup animations added

--- js/ui.js
+++ js/ui.js
@@ -15,7 +15,7 @@ UI.Template = function(templateName){
 		if(template){
 			return template;
 		}else{
-			console.error('UI.Template', 'Template is empty!');
+			console.error('UI.Template', 'Template ' + templateName + ' is empty!');
 		}
 	};
 
@@ -59,7 +59,8 @@ UI.Checker = function(options){
 };
 
 UI.Popup = function(options){
-	var _this = this;
+	var _this = this,
+		animationTime = 300;
 
 	this.$popup = null;
     this.waitingMode = false;
@@ -123,10 +124,72 @@ UI.Popup = function(options){
         }
     };
 
-    this.showMessage = function(text, type){
-        var template = new UI.Template('template-ui-popup-message');
+    var animateAppear = function(done){
+    	if(_this.$popup){
+    		//perspective: '100px',rotateX: '180deg'
+    		var $overlay = _this.$popup,
+    			$window = _this.$popup.find('.popup');
 
-        var className = '';
+	    	$overlay.transition({
+				opacity: 0
+			}, 0);
+
+			$overlay.css({
+				display: 'block'
+			});
+
+			$window.transition({
+				scale: 0,
+				perspective: '1000px',
+				rotateX: '20deg'
+			}, 0);
+
+			$window.css({
+				display: 'block'
+			});
+
+			setTimeout(function(){
+				$overlay.transition({
+					opacity: 1
+				}, animationTime);
+
+				$window.transition({
+					scale: 1,
+					rotateX: '0deg'
+				}, animationTime, 'easeOutBack');
+
+				if(done) done();
+			}, 50);
+		}else{
+			if(done) done();
+		}
+    };
+
+    var animateDisappear = function(done){
+    	if(_this.$popup){
+			var $overlay = _this.$popup,
+				$window = _this.$popup.find('.popup');
+
+	    	$overlay.transition({
+				opacity: 0
+			}, animationTime);
+
+			$window.transition({
+				scale: 0,
+				rotateX: '20deg'
+			}, animationTime, 'easeInBack');
+
+			setTimeout(function(){
+				if(done) done();
+			}, animationTime);
+		}else{
+			if(done) done();
+		}
+    };
+
+    this.showMessage = function(text, type){
+        var template = new UI.Template('template-ui-popup-message'),
+        	className = '';
 
         switch(type){
             case 'error' : className = 'bg-bittersweet-dark'; break;
@@ -180,19 +243,24 @@ UI.Popup = function(options){
 		this.$popup = $(make(title, content));
 
 		$('body').append(this.$popup);
-		this.options.onShow(this);
 
-		bind();
-        resize();
+		animateAppear(function(){
+			bind();
+        	resize();
+
+        	_this.options.onShow(this);
+		});
 	};
 
 	this.hide = function(){
-		unbind();
-
 		if(this.$popup){
-			this.$popup.remove();
-		}
+			animateDisappear(function(){
+				unbind();
 
-		this.options.onHide(this);
+				_this.$popup.remove();
+				_this.options.onHide(this);
+				_this.$popup = null;
+			});
+		}
 	};
 };
